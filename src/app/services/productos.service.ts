9a4cd321d33b456a67d222828e31b1f70f95b203
jesus-rojas <jarojas6524@misena.edu.co>
2021-05-24 19:55:03 -0500

    App lista para subir

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -15,19 +15,37 @@ export class ProductosService {
   }
 
   private cargarProductos () {
-    this.http.get('https://webapp-angular-b2410-default-rtdb.firebaseio.com/productos_idx.json')
+    return new Promise((resolve, reject) => {
+      this.http.get('https://webapp-angular-b2410-default-rtdb.firebaseio.com/productos_idx.json')
       .subscribe( (resp: any) => {
         this.productos=resp;
         this.cargando = false;
+        resolve('success');
       });
+    })
+    
   }
   // https://webapp-angular-b2410-default-rtdb.firebaseio.com/productos/prod-1.json
   getProducto (id : String) {
     return this.http.get(`https://webapp-angular-b2410-default-rtdb.firebaseio.com/productos/${id}.json`);
   }
-  buscarProducto( termino: String){
-    this.productosFiltrados = this.productos.filter( producto =>{
-      return true;
+  async buscarProducto( termino: String){
+    if (this.productos.length === 0) {
+      this.cargarProductos().then(() => {
+        this.filtrarProductos(termino);
+      });
+    }else{
+      this.filtrarProductos(termino);
+    }
+  }
+  private filtrarProductos( termino: String){
+    this.productosFiltrados = [];
+    termino = termino.toLocaleLowerCase();
+    this.productos.forEach( (prod:any) => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if (prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrados.push(prod);
+      }
     });
     console.log(this.productosFiltrados);
   }
